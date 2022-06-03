c2d720b72495c7542687df2d6dd0cce2cfbbfa23
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-06-03 16:03:02 +0000

    home page restored

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import { Book, Construction, Home, Location } from "./pages";
+import { Book, Home, Location } from "./pages";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Construction />} />
+      <Route path="/" element={<Home />} />
       <Route path="/book" element={<Book />} />
       <Route path="/location" element={<Location />} />
     </Routes>
