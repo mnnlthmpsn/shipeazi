f2b96d8368010e5dfb9e11a3a56855e6840b572a
Emmanuel Thompson <e.thompson@appsNmobile.local>
2022-06-30 13:56:37 +0000

    testimonials

--- src/components/home/introSection.js
+++ src/components/home/introSection.js
@@ -1,4 +1,6 @@
 import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/solid";
+import { Carousel } from 'react-responsive-carousel';
+
 import useBookLocation from "../../hooks/useLoc";
 
 export const IntroSection = () => {
@@ -7,7 +9,7 @@ export const IntroSection = () => {
 
   return (
     <div className="container">
-      <div className="flex flex-col items-center justify-center w-full space-y-14">
+      <div className="flex flex-col items-center justify-evenly h-screen w-full lg:pt-12 ">
         <div className="flex flex-col lg:gap-4 items-center justify-center">
           <p className="text-center font-bold text-white text-3xl md:text-4xl lg:text-6xl">Move Anything Hassle Free</p>
           <p className="text-white md:text-3xl lg:font-bold">Fast, Safe & Reliable</p>
@@ -37,25 +39,20 @@ export const IntroSection = () => {
             <button className="btn-quote-sec">Get Quote</button>
           </div>
         </div>
-        {/* <form className="w-full bg-white px-8 pt-4 pb-8 rounded-lg grid lg:grid-cols-5 gap-3 md:gap-x-5" onSubmit={getQuote}>
-          <div className="col-span-5 lg:col-span-2">
-            <div className="flex gap-2 items-center py-2">
-              <ArrowDownIcon className="h-3 w-auto" />
-              <label htmlFor="destination" className="">Origin</label>
-            </div>
-            <input type="text" className="form-control" placeholder="eg. Dansoman SSNIT Flats" onChange={e => add_pickup(e.target.value)} value={pick_up} required />
-          </div>
-          <div className="col-span-5 lg:col-span-2">
-            <div className="flex gap-2 items-center py-2">
-              <ArrowUpIcon className="h-3 w-auto" />
-              <label htmlFor="destination" className="">Destination</label>
-            </div>
-            <input type="text" className="form-control" placeholder="eg. 15 Maseru St. East Legon" onChange={e => add_destination(e.target.value)} value={destination} required />
-          </div>
-          <div className="col-span-5 lg:col-span-1 h-full flex items-end">
-            <button className="btn-quote-sec">Get Quote</button>
-          </div>
-        </form> */}
+        <Carousel showThumbs={false} showArrows={false} autoPlay={true} showStatus={false} infiniteLoop={true} interval={5000} className="hidden md:block" >
+          {
+            [1, 2, 3, 4].map(item => (
+              <div key={item} className="mb-6">
+                <p className="text-white font-light text-center tracking-wide">
+                  I’ve been using Bellhop since 2015 and have never had a bad
+                  experience. Someone asked me recently what company I’m most loyal to,
+                  and this is the first one that came to mind
+                </p>
+                <p className="py-4 text-white font-thin text-sm">Emmanuel Thompson</p>
+              </div>
+            ))
+          }
+        </Carousel>
       </div>
     </div>
   );
--- src/components/home/testimonial.js
+++ src/components/home/testimonial.js
@@ -1,20 +1,28 @@
-import { AnnotationIcon } from "@heroicons/react/solid";
+import { Carousel } from 'react-responsive-carousel';
 
 export const Testimonial = () => {
   return (
-    <div className="bg-gray-50 md:px-32 lg:px-0">
+    <div className="text-gray-800 bg-gray-200 md:px-32 lg:px-0">
       <div className="container">
-        <div className="lg:px-48 flex flex-col space-y-8 lg:space-y-16 items-center">
-          <AnnotationIcon className="text-[#233152] h-8 w-auto lg:h-16" />
-          <p className="text-[#233152] text-lg lg:text-2xl text-center tracking-wide">
-            I’ve been using Bellhop since 2015 and have never had a bad
-            experience. Someone asked me recently what company I’m most loyal to,
-            and this is the first one that came to mind. You guys have found a way
-            to turn what is typically a painful experience (moving) into a
-            relatively easy process because your movers are always fast and
-            professional. Thank you!
-          </p>
-          <p className="text-gray-500 lg:text-lg font-light">- Emmanuel Thompson</p>
+        <p className="text-2xl lg:text-4xl w-full text-center font-bold pt-16 pb-8">Testimonials</p>
+        <div className="m-auto lg:w-2/3 pb-10">
+          <Carousel showThumbs={false} showArrows={false} autoPlay={true} showStatus={false} infiniteLoop={true} interval={5000} >
+            {
+              [1, 2, 3, 4].map(item => (
+                <div key={item} className="mb-8">
+                  <p className="text-[#233152] text-lg font-bold text-center tracking-wide">
+                    I’ve been using Bellhop since 2015 and have never had a bad
+                    experience. Someone asked me recently what company I’m most loyal to,
+                    and this is the first one that came to mind. You guys have found a way
+                    to turn what is typically a painful experience (moving) into a
+                    relatively easy process because your movers are always fast and
+                    professional. Thank you!
+                  </p>
+                  <p className="py-4">Emmanuel Thompson</p>
+                </div>
+              ))
+            }
+          </Carousel>
         </div>
       </div>
     </div>
